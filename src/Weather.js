4985f6c721b0355cf2be1be72369729c319b7c39
ginedou0315 <ginelle.doubek@gmail.com>
2025-04-17 14:28:29 +0200

    Added other JS

--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,7 @@
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
