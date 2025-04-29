6bfceea0f79938ef7d38f258c944012440b871d4
ginedou0315 <ginelle.doubek@gmail.com>
2025-04-29 14:07:22 +0200

    change the CSS and Layout

--- src/Weather.js
+++ src/Weather.js
@@ -7,67 +7,74 @@ import "./Weather.css";
 export default function Weather() {
   let weatherData = {
     city: "Prague",
-    description: "rainy",
+    description: "Mostly Cloudy",
     humidity: "87%",
     wind: "7.2km/h",
-    temperature: "24",
+    precipitation: "15%",
+    temperature: "18",
     date: "Tuesday, 11:30, ",
     icon: "☀️",
   };
   return (
     <div className="Weather">
       <div className="weather-app">
-        <header>
-          <form>
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              required
-              className="search-input"
-            />
-            <input type="submit" value="Search" className="search-button" />
-          </form>
-        </header>
-        <main>
-          <div className="current-weather">
-            <div>
-              <h1 className="current-city">{weatherData.city}</h1>
-              <p className="current-details">
-                <span>{weatherData.date}</span>
-                <span>{weatherData.description}</span>
-                <br />
-                Humidity: <strong>{weatherData.humidity}</strong>, Wind:{" "}
-                <strong>{weatherData.wind}</strong>
-              </p>
-            </div>
-            <div className="current-temperature">
-              <span className="current-temperature-icon">
-                {weatherData.icon}
-              </span>
-              <span className="current-temperature-value">
-                {weatherData.temperature}
-              </span>
-              <span className="current-temperature-unit">°C</span>
-            </div>
+        <form>
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            required
+            className="search-input"
+            autoFocus="on"
+          />
+          <input type="submit" value="Search" className="search-button" />
+        </form>
+        <h1 className="current-city py-3 ps-2">{weatherData.city}</h1>
+        <ul className="current-details">
+          <li>{weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+
+        <div className="row">
+          <div className="col-6">
+            <span className="current-temperature-icon">{weatherData.icon}</span>
+            <span className="current-temperature-value">
+              {weatherData.temperature}
+            </span>
+            <span className="current-temperature-unit">°C</span>
           </div>
-        </main>
-        <footer>
-          This project was coded by{" "}
-          <a href="https://github.com/ginedou0315" target="_blank">
-            Ginelle Doubek, is open-sourced in{" "}
-            <a
-              href="https://github.com/ginedou0315/weather-forecast-app"
-              target="_blank"
-            >
-              Github
-            </a>{" "}
-            and hosted in{" "}
-            <a href="" target="_blank">
-              Netlify
-            </a>
-          </a>
-        </footer>
+          <div className="col-6">
+            <ul className="current-details">
+              <li>
+                Humidity: <strong>{weatherData.humidity}</strong>
+              </li>
+              <li>
+                Wind: <strong>{weatherData.wind}</strong>
+              </li>
+              <li>
+                Precipitation: <strong>{weatherData.precipitation}</strong>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/ginedou0315" target="_blank">
+          Ginelle Doubek
+        </a>
+        , is open-sourced in{" "}
+        <a
+          href="https://github.com/ginedou0315/weather-forecast-app"
+          target="_blank"
+        >
+          Github
+        </a>{" "}
+        and hosted in
+        <a href="#" target="_blank">
+          {" "}
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
